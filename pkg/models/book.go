a49f8692712e32dd189bdd1078111370cece6917
Jhonatan Ruiz <jhonatan.ruiz@jikkosoft.com>
2023-03-14 22:25:47 -0500

    First commit

---
+++ pkg/models/book.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"github.com/akhil/go-bookstore/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connnect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
